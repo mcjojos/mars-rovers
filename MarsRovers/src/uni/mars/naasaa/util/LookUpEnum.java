4d1e476b65610e0927c8c7e1813e88e328a841b2
karanikasG <karanikasG@gmail.com>
2013-07-07 00:04:25 +0000

    Minor updates in comments

--- MarsRovers/src/uni/mars/naasaa/util/LookUpEnum.java
+++ MarsRovers/src/uni/mars/naasaa/util/LookUpEnum.java
@@ -4,23 +4,24 @@ import java.util.logging.Logger;
 
 /**
  * Utility class whose purpose it to offer a way to look up enums, without
- * throwing RuntimeException in case no corresponding Enum value exist.
+ * throwing {@link RuntimeException} in case no corresponding Enum value exist.
  * 
  * @author karanikasg
  * 
  */
 public class LookUpEnum {
 	/**
-	 * Returns the enum constant of the specified enum type with the specified
-	 * name. The difference with the standard Enum.valueOf(java.lang.Class,
-	 * java.lang.String) is that it will return null instead of throwing an
-	 * IllegalArgumentException if the specified enum type has no constant with
-	 * the specified name, or the specified class object does not represent an
-	 * enum type. In that case it will log an info message.
+	 * Returns the {@link Enum} constant of the specified enumType with the
+	 * specified name. The difference with the standard
+	 * Enum.valueOf(java.lang.Class, java.lang.String) is that it will return
+	 * null instead of throwing an {@link IllegalArgumentException} if the
+	 * specified enum type has no constant with the specified name, or the
+	 * specified class object does not represent an enum type. In that case it
+	 * will log an info message.
 	 * 
 	 * @param enumType
 	 * @param id
-	 * @return
+	 * @return the Enum constant
 	 */
 	public static <E extends Enum<E>> E lookup(Class<E> enumType, String name) {
 		E result = null;
